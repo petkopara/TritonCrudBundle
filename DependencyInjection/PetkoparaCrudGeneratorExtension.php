1d28d26ef626d3531e41d53726795b5ca4393aa5
David Peoples <david.a.peoples@gmail.com>
2017-09-10 12:50:02 -0500

    Fixed namespace problem.

--- DependencyInjection/PetkoparaCrudGeneratorExtension.php
+++ DependencyInjection/PetkoparaCrudGeneratorExtension.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace PetkoparaCrudGeneratorBundle\DependencyInjection;
+namespace Petkopara\CrudGeneratorBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
